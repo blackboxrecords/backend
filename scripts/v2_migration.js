269552e6c75b56375b87dd3d80f9cb66a839f00b
Chance Hudson <jchancehud@gmail.com>
2019-08-16 12:34:45 -0500

    switch async error handling to actually work

--- scripts/v2_migration.js
+++ scripts/v2_migration.js
@@ -9,44 +9,44 @@ const Artist = mongoose.model('Artist')
 /**
  * Run this to get separate artist collection and simplified user/artist relation
  **/
-try {
-  ;(async () => {
-    await mongoose.connect(process.env.DB_URI, {
-      connectTimeoutMS: 5000,
-      useNewUrlParser: true,
-    })
-    console.log('connected')
-    const userArtists = await UserArtist.find({}).exec()
-    let count = 0
-    for (const userArtist of userArtists) {
-      console.log(`step ${++count} of ${userArtists.length}`)
-      const existingArtist = await Artist.findOne({
-        name: userArtist.name,
-      }).exec()
-      if (existingArtist) {
-        await UserArtist.findOneAndUpdate(
-          {
-            _id: mongoose.Types.ObjectId(userArtist._id),
-          },
-          {
-            artistId: existingArtist._id,
-          }
-        ).exec()
-        continue
-      }
-      const artist = await Artist.create(userArtist)
+;(async () => {
+  await mongoose.connect(process.env.DB_URI, {
+    connectTimeoutMS: 5000,
+    useNewUrlParser: true,
+  })
+  console.log('connected')
+  const userArtists = await UserArtist.find({}).exec()
+  let count = 0
+  for (const userArtist of userArtists) {
+    console.log(`step ${++count} of ${userArtists.length}`)
+    const existingArtist = await Artist.findOne({
+      name: userArtist.name,
+    }).exec()
+    if (existingArtist) {
       await UserArtist.findOneAndUpdate(
         {
           _id: mongoose.Types.ObjectId(userArtist._id),
         },
         {
-          artistId: artist._id,
+          artistId: existingArtist._id,
         }
-      )
+      ).exec()
+      continue
     }
-    process.exit()
-  })()
-} catch (err) {
-  console.log('Error in migration!', err)
-  process.exit(1)
-}
+    const artist = await Artist.create(userArtist)
+    await UserArtist.findOneAndUpdate(
+      {
+        _id: mongoose.Types.ObjectId(userArtist._id),
+      },
+      {
+        artistId: artist._id,
+      }
+    )
+  }
+  process.exit()
+})()
+  .then(() => console.log('v2 migration completed, exiting'))
+  .catch((err) => {
+    console.log('Error in migration!', err)
+    process.exit(1)
+  })
